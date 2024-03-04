b7b40e6778f44694beaa70bb60f4b6f364998ff4
HassanEssam0110 <hassanessam776@gmail.com>
2024-03-04 14:37:32 +0200

    first commit

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -8,15 +8,15 @@ export default function HomePage() {
   return (
     <>
       <MainSlider />
-      <ProductsSlider
+      {/* <ProductsSlider
         categoryId={"6439d58a0049ad0b52b9003f"}
         title={"products for women's"}
       />
-      <CategoriesSlider />
       <ProductsSlider
         categoryId={"6439d5b90049ad0b52b90048"}
         title={"products for men's"}
-      />
+      /> */}
+      <CategoriesSlider />
       <Products />
       <Helmet>
         <meta charSet="utf-8" />
